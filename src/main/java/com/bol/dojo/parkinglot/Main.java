5295e0dbe8d79313db041812795cde697fb50afc
eykema <eykema@bol.com>
2015-12-11 16:52:52 +0100

    SP50 - our first commit!

---
+++ src/main/java/com/bol/dojo/parkinglot/Main.java
@@ -0,0 +1,10 @@
+package com.bol.dojo.parkinglot;
+
+/**
+ * Created by eykema on 11-12-2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/com/bol/dojo/parkinglot/MyFirstTest.java
@@ -0,0 +1,13 @@
+package com.bol.dojo.parkinglot;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNull;
+
+public class MyFirstTest {
+    @Test
+    public void ShouldYieldTrue() {
+        assertNull(null);
+        System.out.println("onzin!");
+    }
+}
